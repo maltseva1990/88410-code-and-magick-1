8f10463e4d4e80f2d5c8b3eb4e47332de7b659e0
Sveta <smaltseva00@gmail.com>
2017-01-28 15:19:20 +0300

    Учебный проект: одеть Надежду. Добавлены изменения

--- js/setup.js
+++ js/setup.js
@@ -5,11 +5,37 @@ var setup = document.querySelector('.setup');
 var setupClose = setup.querySelector('.setup-close');
 var wizard = document.querySelector('#wizard');
 var wizardCoat = wizard.querySelector('#wizard-coat');
-var wizardCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var wizardCoatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
 var wizarEyes = wizard.querySelector('#wizard-eyes');
-var wizardEyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var wizardEyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
 var fireballWrap = document.querySelector('.setup-fireball-wrap');
-var wizardFireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var wizardFireballColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+var nameField = document.querySelector('.setup-user-name');
+nameField.required = true;
+nameField.maxLength = 50;
+nameField.value = 'Синий Пендальф';
+nameField.type = 'text';
+
+var getRandom = function (min, max) {return Math.floor((Math.random() * (max - min) + min));}
 
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('invisible');
@@ -20,19 +46,13 @@ setupClose.addEventListener('click', function () {
 });
 
 wizardCoat.addEventListener('click', function () {
-  wizardCoat.style.fill = wizardCoatColors[
-    Math.floor(Math.random() * wizardCoatColors.length)
-    ];
+  wizardCoat.style.fill = wizardCoatColors[getRandom(0,5)];
 });
 
 wizarEyes.addEventListener('click', function () {
-  wizarEyes.style.fill = wizardEyesColors [
-    Math.floor(Math.random() * wizardEyesColors .length)
-    ];
+  wizarEyes.style.fill = wizardEyesColors [getRandom(0,5)];
 });
 
 fireballWrap.addEventListener('click', function () {
-  fireballWrap.style.background = wizardFireballColors [
-    Math.floor(Math.random() * wizardFireballColors .length)
-    ];
+  fireballWrap.style.background = wizardFireballColors [getRandom(0,5)];
 });
