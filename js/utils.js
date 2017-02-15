14203c0a8132c29187ab665fa1937a1e137fddcb
Maltseva Svetlana <smaltseva00@gmail.com>
2017-02-16 00:18:09 +0300

    Добавлена инкапсуляция в модуль utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,28 +1,34 @@
 'use strict';
 
-var ENTER_KEY_CODE = 13;
-var ESCAPE_KEY_CODE = 27;
+window.utils = (function(){
+  var ENTER_KEY_CODE = 13;
+  var ESCAPE_KEY_CODE = 27;
 
-window.utils = {
-
-  isActivateEvent: function (evt) {
+  var isActivateEvent = function (evt) {
     return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-  },
+  };
 
-  isDiactivateEvent: function (evt) {
+  var isDiactivateEvent = function (evt) {
     return evt.keyCode && evt.keyCode === ESCAPE_KEY_CODE;
-  },
+  };
 
-  getRandomElement: function (array) {
+  var getRandomElement = function (array) {
     var randomElementIndex = Math.floor(Math.random() * array.length);
     return array[randomElementIndex];
-  },
+  };
 
-  getRandomElementExcept: function (array, colorValue) {
+  var getRandomElementExcept = function (array, colorValue) {
     var randomElement = null;
     while (!randomElement || randomElement === colorValue) {
       randomElement = this.getRandomElement(array);
     }
     return randomElement;
-  }
-};
+  };
+
+  return {
+    isActivateEvent: isActivateEvent,
+    isDiactivateEvent: isDiactivateEvent,
+    getRandomElement: getRandomElement,
+    getRandomElementExcept: getRandomElementExcept
+  };
+})();
